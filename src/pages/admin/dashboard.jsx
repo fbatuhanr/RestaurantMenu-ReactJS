390f42e1af8061bc008878e908cb0df028d320ab
Batuhan Ozturk <fbatuhanr@gmail.com>
2023-09-03 01:26:51 +0300

    update & remove & insert new menu items

--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {Container, Row, Col, Card, Form, Button, Placeholder} from "react-bootstrap"
 
 import {AiFillPlusCircle} from "react-icons/ai"
@@ -6,54 +6,88 @@ import axios from "axios";
 
 const Dashboard = () => {
 
-    const [menu, setMenu] = useState(null);
+    const [menu, setMenu] = useState([]);
+    const [newDataObject, setNewDataObject] = useState([]);
 
-    useEffect(()=>{
 
-        axios.get('http://localhost:3001/categories')
-            .then(result => {
-                setMenu(result.data)
-            })
+    useEffect(() => {
+
+        assignData();
 
     },[]);
 
+    const callData = async () => {
 
-    const [newDataObject, setNewDataObject] = useState([]);
-    const handleNewItemChange = (e, id, slug) => {
-
-        if(newDataObject[0] && newDataObject[0] !== id)
-            setNewDataObject([]);
-
-        const { name, value } = e.target;
-        setNewDataObject(state => [id, ({ ...state[1], [name]: value })])
+        try {
+            const {data} = await axios.get('http://localhost:3001/categories')
+            return data;
+        }
+        catch (error) {
+            console.log(error);
+            return false;
+        }
     }
-    const addNewItem = () => {
-
-        /*setMenu(current => {
-            return {
-                ...current,
-                [newDataObject[0]]: {
-                    ...current[newDataObject[0]],
-                    items: [...current[newDataObject[0]].items, newDataObject[1]]
-                }
-            }
-        })
-        console.log(menu)*/
-
-        axios.put(`http://localhost:3001/categories/${newDataObject[0]}`, newDataObject[1])
-            .then(result=>{
-                console.log(result)
-            })
+    const assignData = () => {
+        callData().then(data => data !== false && setMenu(data))
     }
 
-    const updateJsonData = () => {
-        const json = JSON.stringify(menu, null, 2);
-        const blob = new Blob([json], { type: 'application/json' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'menu.json';
-        a.click();
+
+    const handleAddItemSubmit = (e, keys, id) => {
+
+        e.preventDefault();
+
+        // introduce keys and pass values by form.
+        let newItem = keys.reduce((acc, curr) => {
+            acc[curr] = e.target[curr].value;
+            return acc;
+        }, {});
+
+        e.target.reset();
+
+        //  find related category by id and add new item to items object.
+        let subMenu = menu.find(element => element.id === id);
+        subMenu.items.push(newItem);
+
+        // update json file by new inserted data with related category id after that we assign menu state (for seeing new inserted json data on the screen).
+        axios.put(`http://localhost:3001/categories/${id}`, subMenu)
+            .then(response => {
+                console.log(`Added! Response: ${response}`)
+            assignData();
+        });
+    }
+
+    const handleUpdateSubmit = (e, keys, id, index) => {
+
+        e.preventDefault();
+
+        // introduce keys and pass values by form.
+        let updatedData = keys.reduce((acc, curr) => {
+            acc[curr] = e.target[curr].value;
+            return acc;
+        }, {});
+
+        //  find related category by id and update related index by new one.
+        let subMenu = menu.find(element => element.id === id);
+        subMenu.items[index] = updatedData;
+
+        // update json file by new data with related category id after that assign menu state (for seeing new updated json data on the screen).
+        axios.put(`http://localhost:3001/categories/${id}`, subMenu)
+             .then(response => {
+                    console.log(`Updated! Response: ${response}`)
+                 assignData();
+             });
+    }
+
+    const handleRemoveButton = (id, index) => {
+
+        let subMenu = menu.find(element => element.id === id);
+        subMenu.items.splice(index, 1);
+
+        axios.put(`http://localhost:3001/categories/${id}`, subMenu)
+            .then(response => {
+                console.log(`Removed! Response: ${response}`)
+                assignData();
+            });
     }
 
     return (
@@ -66,9 +100,9 @@ const Dashboard = () => {
                     </Row>
                     <Row>
                         {
-                            menu && menu.map((item) => {
+                            menu && menu.map(element => {
 
-                                    let {id, slug, info, items} = item;
+                                    let {id, slug, info, items} = element;
                                     return (
                                         <Col xs={12} className="my-1">
                                             <Card>
@@ -76,11 +110,9 @@ const Dashboard = () => {
                                                 <Card.Body>
                                                     <Card.Title>
                                                         {
-                                                            Object.entries(info).map(i=>(
+                                                            Object.entries(info).map(i => (
                                                                 <Form.Group as={Row}>
-                                                                    <Form.Label column xs={4}>
-                                                                        {i[0]}:
-                                                                    </Form.Label>
+                                                                    <Form.Label column xs={4}>{i[0]}:</Form.Label>
                                                                     <Col xs={8}>
                                                                         <Form.Control type="text" value={i[1]} />
                                                                     </Col>
@@ -93,28 +125,41 @@ const Dashboard = () => {
                                                     </Card.Title>
                                                     <Card.Text>
                                                         {
-                                                            items.map(i=>(
-                                                                <Col xs={12} className="my-2 border rounded p-2 bg-secondary text-white">
+                                                            items.map((i, inx)=>(
+                                                                <Col key={inx} xs={12} className="my-2 border rounded p-2 bg-secondary text-white">
+                                                                    <Form onSubmit={e => handleUpdateSubmit(e, Object.keys(i), id, inx)}>
                                                                     {
-                                                                        Object.entries(i).map(j=>(
+                                                                        Object.entries(i).map(j => (
                                                                             <Form.Group as={Row} className="my-1">
-                                                                                <Form.Label column xs={3} className="text-end">
-                                                                                    {j[0]}:
-                                                                                </Form.Label>
+                                                                                <Form.Label column xs={3} className="text-end">{j[0]}:</Form.Label>
                                                                                 <Col xs={9}>
-                                                                                    <Form.Control type="text" value={j[1]} />
+                                                                                    <Form.Control
+                                                                                        type="text"
+                                                                                        name={j[0]}
+                                                                                        defaultValue={j[1]}
+                                                                                    />
                                                                                 </Col>
                                                                             </Form.Group>
                                                                         ))
                                                                     }
-                                                                    <div className="d-grid">
-                                                                        <Button variant="danger">Remove</Button>
-                                                                    </div>
+                                                                        <Row className="pt-2 pb-1">
+                                                                            <Col className="px-0">
+                                                                                <Button variant="primary" className="w-100" type="submit">Update</Button>
+                                                                            </Col>
+                                                                            <Col className="px-0">
+                                                                                <Button variant="danger" className="w-100" type="button"
+                                                                                        onClick={()=>handleRemoveButton(id, inx)}>
+                                                                                    Remove
+                                                                                </Button>
+                                                                            </Col>
+                                                                        </Row>
+                                                                    </Form>
                                                                 </Col>
                                                             ))
                                                         }
                                                         {
                                                             <Col xs={12} className="my-2 border rounded p-2 bg-secondary text-white">
+                                                                <Form onSubmit={e => handleAddItemSubmit(e, Object.keys(items[0]), id)}>
                                                                 {
                                                                     // we used first element of 'items' for take reference about object keys
                                                                     Object.entries(items[0]).map((j, inx)=>{
@@ -124,18 +169,20 @@ const Dashboard = () => {
                                                                                     {j[0]}:
                                                                                 </Form.Label>
                                                                                 <Col xs={9}>
-                                                                                    <Form.Control type="text" name={j[0]}
-                                                                                                  value={newDataObject[j[0]]}
-                                                                                                  onChange={e=>handleNewItemChange(e, id, slug)}
+                                                                                    <Form.Control
+                                                                                        type="text"
+                                                                                        name={j[0]}
+                                                                                        defaultValue=""
                                                                                     />
                                                                                 </Col>
                                                                             </Form.Group>
                                                                         )
                                                                     })
                                                                 }
-                                                                <div className="d-grid">
-                                                                    <Button variant="success" onClick={addNewItem}>Add New Item</Button>
-                                                                </div>
+                                                                    <div className="d-grid">
+                                                                        <Button variant="success" type="submit">Add New Item</Button>
+                                                                    </div>
+                                                                </Form>
                                                             </Col>
                                                         }
 
